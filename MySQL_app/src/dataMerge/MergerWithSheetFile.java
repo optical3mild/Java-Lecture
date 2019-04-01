cf24b7c8bfd6c05b3daa451e2a50a9e2c93b9415
optical3mild <chupa.pal.chad@gmail.com>
2019-04-01 17:53:34 +0900

    add plan

---
+++ MySQL_app/src/dataMerge/MergerWithSheetFile.java
@@ -0,0 +1,18 @@
+package dataMerge;
+
+public class MergerWithSheetFile {
+	//파일을 읽어올 것 (띄어쓰기로 구분 or 스프레드 시트파일)
+	
+	//각 셀의 내용(항목)의 앞뒤에 backquote 삽입
+	//각 행의 앞뒤 끝에 ()처리
+	//head 정의와 내용 구별
+	//열만 입력 가능
+	//행만 입력 가능
+	//특정항목만 업데이트...
+	
+	
+	public String merger() {
+		
+		return null;
+	}
+}
