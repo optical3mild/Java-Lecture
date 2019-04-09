04b7bc236b340592d2b4420cae013803aee33645
optical3mild <chupa.pal.chad@gmail.com>
2019-04-09 09:05:56 +0900

    chapter 14

--- OpenChallenge/src/oc_190408/ex01_dart/DartApp.java
+++ OpenChallenge/src/oc_190408/ex01_dart/DartApp.java
@@ -7,7 +7,6 @@ public class DartApp {
 	public static void main(String[] args) {
 		// 1. 객체에 필드 값 입력
 		// 2. option 영역에 따라 점수 계산
-		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Dart 게임 입력> ");
 		String game = scan.nextLine();
@@ -15,64 +14,63 @@ public class DartApp {
 		scan.close();
 	}
 
-	static int dartResult(ScoreBoard[] dart) {
+	static int dartResult(ScoreBoard[] dart) { //parseDart() 메소드에서 정리된 게임정보를 매개변수로 받아 점수계산
 		int score = 0;
-		for (int i=0; i<3; i++) {
+		for (int i=0; i<3; i++) {					//3회 게임에서 받은 점수에 영역(area필드)값을 적용하여 계산하는 loop
 			switch (dart[i].area) {
-			case 'S':
+			case 'S':								//single의 경우 점수가 그대로 반영됨
 				dart[i].score = dart[i].number;
 				break;
-			case 'D':
+			case 'D':								//double의 경우 받은 점수의 제곱으로 반영								
 				dart[i].score = dart[i].number * dart[i].number;
 				break;
-			case 'T':
+			case 'T':								//triple의 경우 받은 점수의 세제곱으로 반영
 				dart[i].score = dart[i].number * dart[i].number * dart[i].number;
 			}
 		}
-		for (int i=0; i<3; i++) {
-			if (dart[i].optionStar) {
+		for (int i=0; i<3; i++) {					//영역값(area)이 반영된 각 회차의 점수에 옵션을 적용하여 계산하는 loop
+			if (dart[i].optionStar) {				//*이 있다면 반영된 점수를 두배로 적용
 				dart[i].score *= 2;
 				if (i != 0) {
-					dart[i-1].score *= 2;
+					dart[i-1].score *= 2;			//*이 있다면 이전 회차의 점수도 두배로 적용
 				}
 			}
-			if (dart[i].optionAcha)
+			if (dart[i].optionAcha)					//#이 있다면 해당 회차의 점수는 -값으로 적용됨
 				dart[i].score *= -1;
 		}
-		score = dart[0].score + dart[1].score + dart[2].score;
+		score = dart[0].score + dart[1].score + dart[2].score;	//최종적으로 1,2,3회차의 점수를 총합하여 리턴
 		return score;
 	}
 	
-	static ScoreBoard[] parseDart(String str) { //
-		ScoreBoard[] dart = new ScoreBoard[3]; //3회의 게임
+	static ScoreBoard[] parseDart(String str) {	//게임 점수 정보를 String 매개변수로 받아 객체의 각 필드에 정리
+		ScoreBoard[] dart = new ScoreBoard[3];	//3회의 게임 -> 매 게임을 배열에 저장
 		StringTokenizer st = null;
-		String s; 
-//		String number = null;
-		int index = 0; 
+		String s;
+		int index = 0; 							//for문 내의 substring()메소드에 이용, 게임 점수 정보를 분할하는 기준점 저장
 		int len = str.length();
 		
 		for (int i=0; i<3; i++) {
-			dart[i] = new ScoreBoard();	//게임이 기록될 객체생성
-			dart[i].optionStar = false;	//*옵션 값 초기화
-			dart[i].optionAcha = false;	//#옵션 값 초기화
-			s = str.substring(index);	//점수 자릿수에 따라 입력된 게임문자열의 나눌 곳을 선택
-			System.out.println(s);
+			dart[i] = new ScoreBoard();			//게임 정보를 저장할 객체 생성
+			dart[i].optionStar = false;			//객체내의 게임 옵션값 초기화
+			dart[i].optionAcha = false;
+			s = str.substring(index);			//index값 부터 시작하여 index값 이후의 게임 점수 정보를 분할하여 String 참조변수에 저장
+			//System.out.println(s);			//중간확인용도
 			
-			st = new StringTokenizer(s, "SDT");
-			dart[i].number = Integer.parseInt(st.nextToken());
-			index += (dart[i].number == 10) ? 2 : 1;
-			dart[i].area = str.charAt(index); 
-			index++;
-			if (index < len) {
-				if (str.charAt(index) == '*')
+			st = new StringTokenizer(s, "SDT");	//index값 이후부터 분할저장된 게임 정보를 구분자(S/D/T)를 기준으로 다시 분할(=token)
+			dart[i].number = Integer.parseInt(st.nextToken()); //저장되어 있는 토큰 중 첫번째를 정수로 변환, 객체i번째 배열의 number필드에 대입
+			index += (dart[i].number == 10) ? 2 : 1;	//점수의 범위는 1-10까지 이므로, 점수가 10일때 index 값을 2증가(두자리 뒤부터 분할하도록)
+			dart[i].area = str.charAt(index); 			//증가된 index값이 가리키는 위치는 S/D/T 이므로 이를 이용하여 S/D/T정보를 객체의 area필드에 저장
+			index++;							//점수와 영역값을 추출하였으므로, 그 이후의 옵션값과 다음회차부터 검사하기 위해 index값을 증가
+			if (index < len) {					//점수기록의 문자열 길이보다 index값이 클 수 없고, 그에따른 예외를 방지하기 위한 조건을 설정
+				if (str.charAt(index) == '*')	//영역 이후 옵션값 확인, 옵션값이 존재한다면 true를 반환
 					dart[i].optionStar = true;
 				if (str.charAt(index) == '#')
 					dart[i].optionAcha = true;
-				if (dart[i].optionStar || dart[i].optionAcha)
+				if (dart[i].optionStar || dart[i].optionAcha) //옵션값이 존재한다면 index값을 증가하여 문자열 내 다음회차 정보의 기준점을 조정
 					index++;
 			}
-			//System.out.println(i + dart[i].toString());
+			//System.out.println(i + dart[i].toString()); //중간 확인용도
 		}
-		return dart;
+		return dart;	//루프 종료 후, 저장된 객체 주소배열을 반환
 	}
 }
--- OpenChallenge/src/oc_190408/ex01_dart/ScoreBoard.java
+++ OpenChallenge/src/oc_190408/ex01_dart/ScoreBoard.java
@@ -1,15 +1,16 @@
 package oc_190408.ex01_dart;
 
 public class ScoreBoard {
-	public int number;
-	public char area;
-	public boolean optionStar;
-	public boolean optionAcha;
-	public int score;
+	public int number;			//입력된 게임정보 중 점수값(score)으로 저장하기 전 보관하는 필드
+	public char area;			//영역 SDT 정보를 저장하는 필드
+	public boolean optionStar;	//*값의 유무를 저장하는 필드
+	public boolean optionAcha;	//#값의 유무를 저장하는 필드
+	public int score;			//각 필드에 저장된 정보를 총합하여 회차마다 계산된 점수를 저장하는 필드
 	
 	@Override
 	public String toString() {
-		return "Dart [number=" + number + ", area=" + area + ", optionStar=" + optionStar + ", optionAcha="
-				+ optionAcha + ", score=" + score + "]";
+		return "Dart [number=" + number + ", area=" + area
+					+ ", optionStar=" + optionStar + ", optionAcha="
+					+ optionAcha + ", score=" + score + "]";
 	}
 }
